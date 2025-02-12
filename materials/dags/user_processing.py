34175f40c899f59acc8dfa32938e48deaae1ce27
Manuel <manuel.hany@umbrasys.com>
2025-02-12 22:35:01 +0200

    added .gitignore

--- materials/dags/user_processing.py
+++ materials/dags/user_processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 from datetime import datetime
 
@@ -10,4 +11,21 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    pass
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        """,
+    )
+
+    is_api_available = HttpSensor(
+        task_id="is_api_available", http_conn_id="user_api", endpoint="api/"
+    )
